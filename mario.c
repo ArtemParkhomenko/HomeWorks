99e3500da82c0d14dba1ee638438eba4c1cbf7c6
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-10 19:44:04 +0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <cs50.h>
+
+
+
+int main(void)
+
+{
+
+    int namber = GetInt ();
+
+    for (int i = 0; i < namber; i++){
+
+        for (int m = 0; m < i; m++){
+
+            printf ("*");
+
+        }
+
+        printf ("\n");
+
+        }
+
+}
---
+++ water.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <cs50.h>
+
+
+
+int main(void)
+
+{
+    
+	int quantity = 0;
+   
+	do{
+
+        printf ("print quantity minutes: ");
+
+        quantity = GetInt ();
+
+        }
+    
+    while (quantity < 1);
+
+        int amount = (quantity * 12);
+
+        printf ("sum bottles: %d\n", amount);
+
+}
