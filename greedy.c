f3081bea2bab75ef25dfe792b38bbe6d13a9ad07
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-13 00:39:17 +0300

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -5,9 +5,8 @@ int main(void)
 {
 
     int cents = 0;      //Необходимая сдача
-    int quantity = 0;   //количество монет
-    int balance = 0;    //остача
     int index = 0;      //количствво монет в остаче
+    int i = 0;
 
     do                  // проверка целое ли число 
     {
@@ -17,15 +16,36 @@ int main(void)
         printf("retype\n:");
     }
     while(cents <= 0); 
-    if (cents > 50){    //Количество по монет номиналом 50
-    for (index = 0; cents == 0; i++){
-        balance = cents - 50;
-        quantity = index + i;
+    
+    if (cents > 50){   //Количество по монет номиналом 50
+        for (i = 0; cents < 49; i++){
+        cents = cents - 50;
+        index = index + i;
         }
     }
-    else if(balance > 25)  //Количество по монет номиналом 25
-    for ( = 0; cents == 0; i++){
-        balance = cents - 25;
-        quantity = i;
-    }
+            else if (cents < 50){   //Количество по монет номиналом 25
+                for (i = 0; cents < 24; i++){
+                cents = cents - 25;
+                index = index + i;
+                }
+            }
+                else if (cents < 25){   //Количество по монет номиналом 10
+                    for (i = 0; cents < 9; i++){
+                    cents = cents - 10;
+                    index = index + i;
+                    }
+                }
+                    else if (cents < 10){   //Количество по монет номиналом 5
+                        for (i = 0; cents < 4; i++){
+                        cents = cents - 5;
+                        index = index + i;
+                        }
+                    }
+                            else if (cents < 5){   //Количество по монет номиналом 1
+                                for (i = 0; cents == 0; i++){
+                                cents = cents - 1;
+                                index = index + i;
+                                }
+                            }
+    printf ("%d\n", index);
 }
