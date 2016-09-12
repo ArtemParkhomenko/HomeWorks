e303868c70d2ec36a6b53f39956ea8345e8518b9
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-12 23:20:48 +0300

    Update mario.c

--- mario.c
+++ mario.c
@@ -4,9 +4,12 @@
 int main(void){
     int namber = GetInt ();
     for (int i = 0; i < namber; i++){
-        for (int m = 0; m < i; m++){
-            printf ("*");
+        for (int m = 0; m < namber-i-1; m++){
+            printf ("%s", " ");
+        }
+        for(int a = 0; a < i+1; a++){
+            printf("*");
         }
         printf ("\n");
-        }
+    } 
 }
