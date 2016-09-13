55def5d1bb3619f26b3dedc04341b8f7975f4410
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-13 16:10:39 +0300

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -20,22 +20,17 @@ int main(void)
         printf("retype\n:");
     }
     while(sum <= 0); 
-    
-    cents25 = sum % 25;
+    cents25 = sum % 25; // количсество монет номинало 25
     index25 = (sum - cents25) / 25;
-    printf ("25 cents coins: %d,\n", index25); // количсество монет номинало 25
-    
-    cents10 = cents25 % 10;
+    printf ("25 cents coins: %d,\n", index25); 
+    cents10 = cents25 % 10; // количсество монет номинало 10
     index10 = (cents25 - cents10) / 10;
-    printf ("10 cents coins: %d,\n", index10); // количсество монет номинало 10
-    
-    cents5 = cents10 % 5;
+    printf ("10 cents coins: %d,\n", index10); 
+    cents5 = cents10 % 5; // количсество монет номинало 5
     index5 = (cents10 - cents5) / 5;
-    printf ("5 cents coins: %d,\n", index5); // количсество монет номинало 5
-    
-    cents1 = cents5 % 1;
+    printf ("5 cents coins: %d,\n", index5); 
+    cents1 = cents5 % 1; // количсество монет номинало 1
     index1 = (cents5 - cents1);
-    printf ("1 cents coins: %d,\n", index1); // количсество монет номинало 1
-    
+    printf ("1 cents coins: %d,\n", index1); 
     printf ("coints sum: %d\n", index25 + index10 + index5 + index1); // выводит общее количество монет
 }
