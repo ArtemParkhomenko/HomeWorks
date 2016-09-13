52d7cd7e2f930ad917b89a911ed97a51712121c3
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-13 15:44:27 +0300

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -3,14 +3,15 @@
 
 int main(void) 
 {
-    int sum = 0;
-    int cents25 = 0;
+    int sum = 0;  // сумма сдачи
+    int cents25 = 0; // остаток от деления на 25
     int cents10 = 0;
     int cents5 = 0;
     int cents1 = 0;
-    int index = 0;
-    int index10 = 0; 
-
+    int index25 = 0; // количсество монет номинало 25
+    int index10 = 0;
+    int index5 = 0;
+    int index1 = 0;
     do                  // проверка на целое число больше ноля 
     {
         printf("You gave me number of cents: "); 
@@ -20,19 +21,21 @@ int main(void)
     }
     while(sum <= 0); 
     
-    cents25 = sum % 25; // определение количества монет номиналом 25
-    index = (sum - cents25) / 25;
-    printf ("25 cents coins: %d,\n", index);
+    cents25 = sum % 25;
+    index25 = (sum - cents25) / 25;
+    printf ("25 cents coins: %d,\n", index25); // количсество монет номинало 25
     
-    cents10 = cents25 % 10;   // определение количества монет номиналом 10
+    cents10 = cents25 % 10;
     index10 = (cents25 - cents10) / 10;
-    printf ("10 cents coins: %d,\n", index10);
+    printf ("10 cents coins: %d,\n", index10); // количсество монет номинало 10
     
-    cents5 = cents10 % 5;   // определение количества монет номиналом 5
-    index = (cents10 - cents5) / 5;
-    printf ("5 cents coins: %d,\n", index);
+    cents5 = cents10 % 5;
+    index5 = (cents10 - cents5) / 5;
+    printf ("5 cents coins: %d,\n", index5); // количсество монет номинало 5
     
-    cents1 = cents5 % 1;   // определение количества монет номиналом 1
-    index = (cents5 - cents1);
-    printf ("1 cents coins: %d,\n", index);
+    cents1 = cents5 % 1;
+    index1 = (cents5 - cents1);
+    printf ("1 cents coins: %d,\n", index1); // количсество монет номинало 1
+    
+    printf ("coints sum: %d\n", index25 + index10 + index5 + index1); // выводит общее количество монет
 }
