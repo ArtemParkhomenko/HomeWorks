991c0669465bcb22b3e4d35ce6d04e104c201295
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-27 21:38:49 +0300

    Update water.c

--- water.c
+++ water.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
-
 #include <cs50.h>
 
-
-
-int main(void)
-
-{
-    
+int main(void){
 	int quantity = 0;
-   
 	do{
-
         printf ("print quantity minutes: ");
-
         quantity = GetInt ();
-
         }
-    
-    while (quantity < 1);
-
+   	while (quantity < 1);
         int amount = (quantity * 12);
-
         printf ("sum bottles: %d\n", amount);
-
 }
