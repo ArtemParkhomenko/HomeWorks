c8b8b12d951132e3f4db5e3c022f1aca7e15ae88
ArtemParkhomenko <aptik.com@gmail.com>
2016-09-27 22:04:34 +0300

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void){
-    int namber = GetInt ();
-    for (int i = 0; i < namber; i++){
-        for (int m = 0; m < namber-i-1; m++){
-            printf ("%s", " ");
-        }
-        for(int a = 0; a < i+1; a++){
-            printf("*");
-        }
-        printf ("\n");
-    } 
+
+void buildingPiramid (int number);  // прототипы функций
+int validNumber(int number);
+
+int main(){  
+    int number = 0;
+    buildingPiramid (validNumber(number));
 }
+
+int validNumber(int number){
+    do{ // проверка на целое число больше ноля и не больше 23
+        printf ("height: ");
+        number = GetInt();
+    }
+    while(number < 0||number > 23); 
+    return number;
+}
+
+void buildingPiramid (int number){ // построение пирамиды
+    for(int i = 1; i <= number; i++){
+        for(int j = 1; j <= number - i; j++){
+            printf(" ");
+        }
+        for(int j = 1; j <= i + 1; j++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
